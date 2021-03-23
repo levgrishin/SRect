9803e81d67fdb65220880200ec5f252cbdb6ee8b
Lev (Arslan) <l.grishin@yandex.ru>
2021-03-23 07:26:16 +0300

    end of themes

--- src/SRect.java
+++ src/SRect.java
@@ -1,7 +1,7 @@
 public class SRect {
     public static void main(String[] args) {
         SRect rect = new Rect();
-        rect.setW() = 8;
+        rect.setW() = 802001001;
         if (rect.getW(10,10) * rect.getH(100,100) == rect.getS(5,5));
 
     }
